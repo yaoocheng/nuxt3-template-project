2cc9cd608f16efde64793ccd68910021fb86c733
yaochengs <1766862282@qq.com>
2023-05-25 11:35:32 +0800

    描述修改

--- composables/utils.ts
+++ composables/utils.ts
@@ -1,21 +1,6 @@
 import { ElNotification } from 'element-plus';
 import Clipboard from 'clipboard';
 
-/**
- * 格式化日期
- */
- export function formatDate(date: string): string {
-    const dat = new Date(date);
-    const year = dat.getFullYear();
-    const mon = (dat.getMonth() + 1) < 10 ? `0${dat.getMonth() + 1}` : dat.getMonth() + 1;
-    const data = dat.getDate() < 10 ? `0${dat.getDate()}` : dat.getDate();
-    const hour = dat.getHours() < 10 ? `0${dat.getHours()}` : dat.getHours();
-    const min = dat.getMinutes() < 10 ? `0${dat.getMinutes()}` : dat.getMinutes();
-    const sec = dat.getSeconds() < 10 ? `0${dat.getSeconds()}` : dat.getSeconds();
-
-    const newDate = `${year}-${mon}-${data} ${hour}:${min}:${sec}`;
-    return newDate;
-}
 
 /**
  * 信息提示
@@ -30,41 +15,6 @@ import Clipboard from 'clipboard';
 }
 
 /**
- * 防抖
- */
- export function debounce(cb: any, time: number) {
-    let timer: any;
-    return () => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            cb();
-        }, time);
-    };
-}
-
-
-/**
- * 操作本地存储信息
- */
-export function storageHandle(type: string, name: string, data: any): any {
-    if (type === 'get') {
-        return JSON.parse(window.localStorage.getItem(name) as string);
-    }
-    if (type === 'set') {
-        window.localStorage.setItem(name, JSON.stringify(data));
-        return;
-    }
-    if (type === 'remove') {
-        window.localStorage.removeItem(name);
-        return;
-    }
-    if (type === 'clear') {
-        window.localStorage.clear();
-    }
-}
-
-
-/**
  * 复制
  */
 export function copyFunc(domSelect: string) {
@@ -73,13 +23,6 @@ export function copyFunc(domSelect: string) {
     return clip;
 }
 
-/**
- * 获取用户信息
- */
- export function getUserInfo() {
-    const userInfo = window.localStorage.getItem('userInfo');
-    return userInfo ? JSON.parse(userInfo) : null;
-}
 
 
 
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,9 @@
     <NuxtLayout name="default">
         <HelloComp />
         <div class="mid">
-            <img style="width: 100px;margin-bottom: 20px;" src="~@/assets/img/logo.png" alt="">
-            <h1>欢迎使用<a class="ye-cli" target="_blank" href="https://github.com/yc-cooding/ye-cli">ye-cli</a>搭建项目</h1>
             <h2>本项目基于Nuxt3，用于构建SSR应用</h2>
             <h2>各个模块都已配置好，搭建后即可进行开发</h2>
         </div>
-
     </NuxtLayout>
 </template>
 
